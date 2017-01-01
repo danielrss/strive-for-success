0302bfab8d9d876323bf265333879c6e6266a76a
Daniel Isov <danielisov96@gmail.com>
2017-01-01 19:11:39 +0200

    added age to registration form and improved validation

--- client/app/user/register/register.component.ts
+++ client/app/user/register/register.component.ts
@@ -11,10 +11,15 @@ import { UserService, UsersFactoryService, AlertService } from '../../../core/se
     styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+    private readonly LETTERS_PATTERN: RegExp = /^[A-Za-zА-Яа-я]+$/;
+    private readonly EMAIL_PATTERN: RegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    private readonly NUMBER_PATTERN: RegExp = /^[0-9]+$/;
+
     public form: FormGroup;
     public firstName: AbstractControl;
     public lastName: AbstractControl;
     public email: AbstractControl;
+    public age: AbstractControl;
     public password: AbstractControl;
     public repeatPassword: AbstractControl;
     public passwords: FormGroup;
@@ -29,9 +34,10 @@ export class RegisterComponent implements OnInit {
             private userFactory: UsersFactoryService,
             private alertService: AlertService) {
         this.form = fb.group({
-          'firstName': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-          'lastName': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-          'email': ['', Validators.compose([Validators.required])],
+          'firstName': ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.pattern(this.LETTERS_PATTERN)])],
+          'lastName': ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.pattern(this.LETTERS_PATTERN)])],
+          'age': ['', Validators.compose([Validators.required, Validators.pattern(this.NUMBER_PATTERN)])],
+          'email': ['', Validators.compose([Validators.required, Validators.pattern(this.EMAIL_PATTERN)])],
           'passwords': fb.group({
             'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
             'repeatPassword': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
@@ -40,6 +46,7 @@ export class RegisterComponent implements OnInit {
 
         this.firstName = this.form.controls['firstName'];
         this.lastName = this.form.controls['lastName'];
+        this.age = this.form.controls['age'];
         this.email = this.form.controls['email'];
         this.passwords = <FormGroup> this.form.controls['passwords'];
         this.password = this.passwords.controls['password'];
@@ -71,7 +78,7 @@ export class RegisterComponent implements OnInit {
                     const successMessage = 'Registration successful!';
                     this.alertService.success(successMessage);
                   }, error => {
-                    const errorMessage = "Such user already exists!";
+                    const errorMessage = "A user with the same email already exists!";
                     this.alertService.error(errorMessage);
                   }, () => {
                      setTimeout(() => this.router.navigateByUrl('/login'), 2500);
