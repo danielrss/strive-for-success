eb60dd9805ef991ccce65a044ec43a364fea529f
Daniel Isov <danielisov96@gmail.com>
2017-01-03 19:49:30 +0200

    added uglify plugin in production config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -19,7 +19,7 @@ module.exports = webpackMerge(commonConfig, {
 
   plugins: [
     new webpack.NoErrorsPlugin(),
-    // new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'ENV': JSON.stringify(ENV)
